b3e906bd8d27b6a2064af3b9742ea48f563182f8
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-10-03 15:28:15 +0700

    .commit

--- pages/4_New.py
+++ pages/4_New.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 
 st.header("Show Data Index Price")
-df=pd.read_csv(".\data\stock_index_price.csv")
+df=pd.read_csv("C:\Users\wiriy\OneDrive\เดสก์ท็อป\เรียน Online\โฟลเดอร์ใหม่\project01\data\stock_index_price.csv")
 st.write(df.head(10))
 
 
