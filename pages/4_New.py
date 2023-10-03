d50ec72fac80a2a3ce622b03d6a62c2f4061573a
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-10-03 15:43:41 +0700

    Update 4_New.py

--- pages/4_New.py
+++ pages/4_New.py
@@ -8,3 +8,7 @@ import pandas as pd
 st.header("Show Data Index Price")
 df=pd.read_csv("./data/stock_index_price.csv")
 st.write(df.head(10))
+
+st.line_chart(
+   chart_data, x="stock_index_price", y=["interest_rate", "unemployment_rate"], color=["#FF0000", "#0000FF"]  # Optional
+)
