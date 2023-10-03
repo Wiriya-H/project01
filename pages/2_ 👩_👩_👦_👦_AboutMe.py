b87bbe2d9675da059ebbd2717d2e93d1ee05cb90
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-10-03 14:57:03 +0700

    .commit

--- pages/2_ 👩_👩_👦_👦_AboutMe.py
+++ pages/2_ 👩_👩_👦_👦_AboutMe.py
@@ -11,7 +11,7 @@ def load_lottieurl(url: str):
         return None
     return r.json()
 
-lottie_url_hello = "https://lottie.host/3f647b41-61bf-4d39-93c3-0433420604cc/8NtmEbAWmO.json"
+lottie_url_hello = "https://lottie.host/40090a00-c7b6-409d-9dad-0a212d961cf7/5imdXjSp0j.json"
 lottie_hello = load_lottieurl(lottie_url_hello)
 st_lottie(lottie_hello, key="hello")
 
