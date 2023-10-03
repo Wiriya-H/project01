67a06e13ce6515a49b8d83578fe3c5953e21cb21
Wiriya-H <119985535+Wiriya-H@users.noreply.github.com>
2023-10-03 15:25:43 +0700

    .commit

--- pages/4_New.py
+++ pages/4_New.py
@@ -3,10 +3,10 @@ import time
 import requests
 import streamlit as st
 import pandas as pd
-from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
+
 
 st.header("Show Data Index Price")
-df=pd.read_csv(".\data\stock_index_price-2.csv")
+df=pd.read_csv(".\data\stock_index_price.csv")
+st.write(df.head(10))
 
 
